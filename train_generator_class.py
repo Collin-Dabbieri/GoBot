e9bc7dfb9ea4e97ed2e838e6bfce8c4e34c6c3d1
Collin Dabbieri <collin.m.dabbieri@vanderbilt.edu>
2021-02-15 19:13:45 -0600

    added model training script

---
+++ train_generator_class.py
@@ -0,0 +1,62 @@
+# Some notes about alphago's RL policy network
+
+# The strong policy network is a 13-layer convolutional network.
+# All of these layers produce 19 × 19 filters;
+# you consistently keep the original board size across the whole network.
+# For this to work, you need to pad the inputs accordingly
+# The first convolutional layer has a kernel size of 5,
+# and all following layers work with a kernel size of 3.
+# The last layer uses softmax activations and has one output filter,
+# and the first 12 layers use ReLU activations and have 192 output filters each.
+
+
+# Each file in the training data contains one complete game of Go
+# This generator will treat each game as a single batch
+# So basically the size of each batch will vary each time
+
+import numpy as np
+import tensorflow as tf
+import math
+
+class DataGenerator(tf.keras.utils.Sequence):
+    'Generates data for Keras'
+    
+    def __init__(self,filenames,shuffle=True):
+        'Initialization'
+        self.filenames=filenames
+        self.shuffle=shuffle #should be false on validation generator so model.predict will return correct indices
+        self.indexes=np.arange(len(filenames))
+        self.on_epoch_end()
+        
+    def __len__(self):
+        'Denotes the number of batches per epoch'
+        # since each batch is a single game of Go, the number of batches is just the number of games of Go created
+        return len(self.filenames)
+    
+    def on_epoch_end(self):
+        'Updates the indexes after each epoch'
+        self.indexes=np.arange(len(self.filenames))
+        if self.shuffle:
+            np.random.shuffle(self.indexes)
+
+    def __getitem__(self,index):
+        'Generate one batch of data'
+        
+        #the generator will provide the index
+                
+        #Generate data
+        X,y=self.__data_generation(self.filenames[index])
+        
+        return X,y
+
+    def __data_generation(self,filename):
+        'Generates data from one game'
+        
+        data=np.load(filename,allow_pickle=True)
+        ins=data['ins'] #shape num_moves,19,19,2
+        outs=data['outs'] #shape num_moves,19*19
+        
+        # we actually want our model output to be a 19x19 matrix so we need to reshape outs
+        outs=outs.reshape((19,19))
+
+
--- train_model.py
+++ train_model.py
@@ -9,4 +9,189 @@
 # The last layer uses softmax activations and has one output filter,
 # and the first 12 layers use ReLU activations and have 192 output filters each.
 
-# There are 8 planes for liberties because the number of liberties is split across 8 binary planes
+
+
+import argparse
+import numpy as np
+import pickle
+import os
+import sys
+import tensorflow as tf
+backend='tensorflow'
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras import optimizers
+from tensorflow.keras import Input
+from tensorflow.keras.models import Model
+import math
+
+ 
+from train_generator_class import DataGenerator as DataGeneratorTrain
+
+########################## USER SPECIFIED VALUES ###############################################
+
+conv=[50,50,50,50,50,50,50,50,50,50] # list with number of filters in each convolutional layer (does not include output layer)
+epochs=10                            # number of training epochs
+activation='elu'                     # activation function for convolutional filters
+l2=0.001                             # l2 regularization value
+kernel_initializer='glorot_uniform'  # kernel initializer
+bias_initializer='zeros'             # bias initializer
+padding='same'                       # padding for convolutional layers
+output_activation='softmax'          # activation function for output convolutional layer
+verbose=1                            # verbosity
+kernel_size=[5,3,3,3,3,3,3,3,3,3]    # list of kernel sizes for each convolutional layer
+loss='categorical_crossentropy'      # loss function for training
+opt='adam'                           # optimizer for training (adam or rmsprop)
+lrate=0.0001                         # learning rate for training
+shuffle=True                         # boolean for shuffling training indexes each epoch
+ 
+files_dir=''                         # path to directory where training data, validation data, and partition file are located
+filename_partition=''                # filename of partition file
+results_dir=''                       # path to directory for saving results
+
+
+########################## FUNCTIONS ###########################################################
+
+def build_model(args):
+    '''
+    Builds the CNN architecture
+    params:
+        args - dictionary containing all gridsearch hyperparams
+    returns:
+        model - tensorflow model object  
+    '''
+    num_layers=len(args['conv'])
+    
+    # build input convolutional layer
+    inpt = Input(shape=(19,19,2),name='input') # 2 filter input Go board
+    x1=Conv2D(filters=args['conv'][0],
+              kernel_size=args['kernel_size'][0],
+              padding=args['padding'],
+              activation=args['activation'],
+              kernel_regularizer=tf.keras.regularizers.l2(args['l2']))(inpt)
+    # build all subsequent convolutional layers
+    for i in range(1,num_layers):
+        x1=Conv2D(filters=args['conv'][i],
+                  kernel_size=args['kernel_size'][i],
+                  padding=args['padding'],
+                  activation=args['activation'],
+                  kernel_initializer=args['kernel_initializer'],
+                  use_bias=True,
+                  bias_initializer=args['bias_initializer'],
+                  kernel_regularizer=tf.keras.regularizers.l2(args['l2']))(x1)
+        
+    # build output convolutional layer
+    output=Conv2D(filters=1,
+                  name='output',
+                  kernel_size=3,
+                  padding='same',
+                  activation=args['output_activation'],
+                  use_bias=True,
+                  bias_initializer=args['bias_initializer'],
+                  kernel_initializer=args['kernel_initializer'])(x1)
+    
+    model=Model(inputs=inpt,outputs=output)
+    
+    if args.opt=="adam":
+        opt=tf.keras.optimizers.Adam(lr=args.lrate,beta_1=0.9,beta_2=0.999,epsilon=None,decay=0.0,amsgrad=False)
+    elif args.opt=="rmsprop":
+        opt="rmsprop"
+        
+    model.compile(loss=args.loss, optimizer=opt, metrics=['categorical_accuracy'])
+    
+    if args.verbose>0.:
+        print(model.summary())
+        
+    return model
+
+def generate_fname(args):
+    '''
+    This takes important model arguments and uses them to generate a filename base for the saved trained model and output pickle files
+    
+    params:
+    args - dictionary object with all model hyperparameters
+    returns:
+    fbase - base filename to be used for trained model and output pickle file
+    '''
+
+    #here we want the filename to (ideally) express all of the information that might be changed from one experiment to the next
+    
+    conv_str='conv_'
+    for i in args['conv']:
+        conv_str=conv_str+str(i).zfill(2)+'_'
+        
+    l2_str='l2_'+str(args['l2']).zfill(6)+'_'
+    lrate_str='lrate_'+str(args['lrate']).zfill(6)
+           
+    fbase=conv_str+l2_str+lrate_str
+    
+    return(fbase)
+
+########################## MAIN LOOP ###########################################################
+
+
+if __name__=='__main__':
+    
+    # build args from values in USER SPECIFIED VALUES
+    args={}
+    args['conv']=conv
+    args['epochs']=epochs
+    args['activation']=activation
+    args['l2']=l2
+    args['kernel_initializer']=kernel_initializer
+    args['bias_initializer']=bias_initializer
+    args['padding']=padding
+    args['output_activation']=output_activation
+    args['verbose']=verbose
+    args['kernel_size']=kernel_size
+    args['loss']=loss
+    args['opt']=opt
+    args['lrate']=lrate
+    args['shuffle']=shuffle
+    
+    if args.verbose>0:
+        print("Building partition")
+        
+    partition=pickle.load(open(files_dir+filename_partition,"rb"))
+    size_train=len(partition['train'])
+    size_validation=len(partition['validation'])
+    
+    #load in generators for training and validation
+    training_generator = DataGeneratorTrain(filenames=partition['train'],
+                                            shuffle=args.shuffle)
+    validation_generator = DataGeneratorTrain(filenames=partition['validation'],
+                                              shuffle=False) #there's no need to shuffle validation indexes
+    
+    if args.verbose>0:
+        print("Building model")
+        
+    model=build_model(args)
+    num_params=model.count_params()
+    
+    if args.verbose>0:
+        print("Training model")
+        
+    fbase=generate_fname(args)
+    
+    #train over synthetic training data
+    history=model.fit_generator(generator=training_generator,
+                                validation_data=validation_generator,
+                                epochs=args['epochs'],
+                                use_multiprocessing=False, # can't overcome the error where memory fills up when using multiprocessing
+                                verbose=args['verbose'])
+    
+    if args.verbose>0:
+        print("Writing Results")
+        
+    results={}
+    results['args']=args
+    results['history']=history.history
+    results['num_params']=num_params
+    
+    #save results
+    results['fname_base']=fbase
+    fp=open("{:s}{:s}_results.pkl".format(results_dir,fbase),"wb")
+    pickle.dump(results,fp)
+    fp.close()
+    
+    #save model
+    model.save("{:s}{:s}_model.hdf5".format(results_dir,fbase))
